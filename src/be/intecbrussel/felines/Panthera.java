ba7325ceb7857cf2fcda6f2e2d4147199cb134fa
Rawnaq <Rawnaq.Albayati@student.intecbrussel.be>
2022-05-10 23:57:47 +0200

    first commit

---
+++ src/be/intecbrussel/felines/Panthera.java
@@ -0,0 +1,16 @@
+package be.intecbrussel.felines;
+
+public class Panthera extends Animal {
+
+
+    public String roar()
+    {
+        return "roar";
+    }
+
+    public void hunt()
+    {
+        System.out.println("Panthera is hunting");
+    }
+
+}
